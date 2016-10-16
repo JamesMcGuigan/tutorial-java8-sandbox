c1ff33af6c87688e2d8635e3d09afe2726956795
JamesMcGuigan <james.mcguigan@gmail.com>
2016-10-17 00:27:22 +0100

    Add mockito tests

--- src/test/java/com/company/LongLoopTest.java
+++ src/test/java/com/company/LongLoopTest.java
@@ -1,20 +1,36 @@
 package com.company;
 
+import static org.mockito.Mockito.*;
 import junit.framework.*;
-import com.company.LongLoop;
 
 public class LongLoopTest extends TestCase {
     protected LongLoop looper;
-    protected final long answer = 2305843005992468481L;
+    protected final long expected = 2305843005992468481L;
 
     protected void setUp(){
-        looper = new LongLoop();
+        looper = spy(new LongLoop());
     }
 
+    public void testVoidLittleLong() {
+        looper.voidLittleLong();
+
+        verify(looper, times(1)).voidLittleLong();
+        verify(looper, times(1)).littleLong();
+        verify(looper, times(0)).voidBigLong();
+        verify(looper, times(0)).bigLong();
+    }
+    public void testVoidBigLong() {
+        looper.voidBigLong();
+
+        verify(looper, times(0)).voidLittleLong();
+        verify(looper, times(0)).littleLong();
+        verify(looper, times(1)).voidBigLong();
+        verify(looper, times(1)).bigLong();
+    }
     public void testLittleLong() {
-        assertEquals(looper.littleLong(), answer);
+        assertEquals(looper.littleLong(), expected);
     }
     public void testBigLong() {
-        assertEquals((long) looper.bigLong(), answer);
+        assertEquals((long) looper.bigLong(), expected);
     }
 }
